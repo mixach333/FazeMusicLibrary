ed3710307a116906ac68333cc306beb8d13902ef
michaelhosh <mixach333@gmail.com>
2023-12-20 13:02:34 +0200

    version correction

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fazestore.music.library"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.4-alpha"
+        versionCode = 1
+        versionName = "1.0.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
